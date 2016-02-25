ebb7526c084944cac7ffe2355fe5b78485641490
marcinbelczewski <marcin.belczewski@ihs.com>
2016-02-25 16:26:48 +0100

    replaced tabs with whitespaces

--- js/main.js
+++ js/main.js
@@ -1,183 +1,183 @@
 jQuery(function($) {'use strict';
 
-	// Navigation Scroll
-	$(window).scroll(function(event) {
-		Scroll();
-	});
+    // Navigation Scroll
+    $(window).scroll(function(event) {
+        Scroll();
+    });
 
 
-	$('.slowscroll a').on('click', function(e) {
-		e.preventDefault();
-		$('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
-		return false;
-	});
+    $('.slowscroll a').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
+        return false;
+    });
 
-	// User define function
-	function Scroll() {
-		var contentTop   =   [];
-		var contentBottom   =   [];
-		var winTop      =   $(window).scrollTop();
-		var rangeTop    =   200;
-		var rangeBottom =   500;
-		$('.navbar-collapse').find('.scroll a').each(function(){
-			var offTop = $( $(this).attr('href') ).offset().top;
-			contentTop.push(offTop);
-			contentBottom.push(offTop + $( $(this).attr('href') ).height() );
-		})
-		$.each( contentTop, function(i){
-			if ( winTop > contentTop[i] - rangeTop ){
-				$('.navbar-collapse li.scroll')
-				.removeClass('active')
-				.eq(i).addClass('active');
-			}
-		})
-	};
-
-
-	// accordian
-	$('.accordion-toggle').on('click', function(){
-		$(this).closest('.panel-group').children().each(function(){
-		$(this).find('>.panel-heading').removeClass('active');
-		 });
-
-	 	$(this).closest('.panel-heading').toggleClass('active');
-	});
-
-	//Slider
-	$(document).ready(function() {
-		var time = 7; // time in seconds
-
-	 	var $progressBar,
-	      $bar,
-	      $elem,
-	      isPause,
-	      tick,
-	      percentTime;
-
-	    //Init the carousel
-	    $("#main-slider").find('.owl-carousel').owlCarousel({
-	      slideSpeed : 500,
-	      paginationSpeed : 500,
-	      singleItem : true,
-	      navigation : true,
-			navigationText: [
-			"<i class='fa fa-angle-left'></i>",
-			"<i class='fa fa-angle-right'></i>"
-			],
-	      afterInit : progressBar,
-	      afterMove : moved,
-	      startDragging : pauseOnDragging,
-	      //autoHeight : true,
-	      transitionStyle : "fadeUp"
-	    });
-
-	    //Init progressBar where elem is $("#owl-demo")
-	    function progressBar(elem){
-	      $elem = elem;
-	      //build progress bar elements
-	      buildProgressBar();
-	      //start counting
-	      start();
-	    }
-
-	    //create div#progressBar and div#bar then append to $(".owl-carousel")
-	    function buildProgressBar(){
-	      $progressBar = $("<div>",{
-	        id:"progressBar"
-	      });
-	      $bar = $("<div>",{
-	        id:"bar"
-	      });
-	      $progressBar.append($bar).appendTo($elem);
-	    }
-
-	    function start() {
-	      //reset timer
-	      percentTime = 0;
-	      isPause = false;
-	      //run interval every 0.01 second
-	      tick = setInterval(interval, 10);
-	    };
-
-	    function interval() {
-	      if(isPause === false){
-	        percentTime += 1 / time;
-	        $bar.css({
-	           width: percentTime+"%"
-	         });
-	        //if percentTime is equal or greater than 100
-	        if(percentTime >= 100){
-	          //slide to next item
-	          $elem.trigger('owl.next')
-	        }
-	      }
-	    }
-
-	    //pause while dragging
-	    function pauseOnDragging(){
-	      isPause = true;
-	    }
-
-	    //moved callback
-	    function moved(){
-	      //clear interval
-	      clearTimeout(tick);
-	      //start again
-	      start();
-	    }
-	});
-
-	//Initiat WOW JS
-	new WOW().init();
+    // User define function
+    function Scroll() {
+        var contentTop   =   [];
+        var contentBottom   =   [];
+        var winTop      =   $(window).scrollTop();
+        var rangeTop    =   200;
+        var rangeBottom =   500;
+        $('.navbar-collapse').find('.scroll a').each(function(){
+            var offTop = $( $(this).attr('href') ).offset().top;
+            contentTop.push(offTop);
+            contentBottom.push(offTop + $( $(this).attr('href') ).height() );
+        })
+        $.each( contentTop, function(i){
+            if ( winTop > contentTop[i] - rangeTop ){
+                $('.navbar-collapse li.scroll')
+                .removeClass('active')
+                .eq(i).addClass('active');
+            }
+        })
+    };
 
 
-	$(document).ready(function() {
-		//Animated Progress
-		$('.progress-bar').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
-			if (visible) {
-				$(this).css('width', $(this).data('width') + '%');
-				$(this).unbind('inview');
-			}
-		});
+    // accordian
+    $('.accordion-toggle').on('click', function(){
+        $(this).closest('.panel-group').children().each(function(){
+        $(this).find('>.panel-heading').removeClass('active');
+         });
 
-		//Animated Number
-		$.fn.animateNumbers = function(stop, commas, duration, ease) {
-			return this.each(function() {
-				var $this = $(this);
-				var start = parseInt($this.text().replace(/,/g, ""));
-				commas = (commas === undefined) ? true : commas;
-				$({value: start}).animate({value: stop}, {
-					duration: duration == undefined ? 1000 : duration,
-					easing: ease == undefined ? "swing" : ease,
-					step: function() {
-						$this.text(Math.floor(this.value));
-						if (commas) { $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")); }
-					},
-					complete: function() {
-						if (parseInt($this.text()) !== stop) {
-							$this.text(stop);
-							if (commas) { $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")); }
-						}
-					}
-				});
-			});
-		};
+         $(this).closest('.panel-heading').toggleClass('active');
+    });
 
-		$('.animated-number').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
-			var $this = $(this);
-			if (visible) {
-				$this.animateNumbers($this.data('digit'), false, $this.data('duration'));
-				$this.unbind('inview');
-			}
-		});
-	});
+    //Slider
+    $(document).ready(function() {
+        var time = 7; // time in seconds
+
+         var $progressBar,
+          $bar,
+          $elem,
+          isPause,
+          tick,
+          percentTime;
+
+        //Init the carousel
+        $("#main-slider").find('.owl-carousel').owlCarousel({
+          slideSpeed : 500,
+          paginationSpeed : 500,
+          singleItem : true,
+          navigation : true,
+            navigationText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+            ],
+          afterInit : progressBar,
+          afterMove : moved,
+          startDragging : pauseOnDragging,
+          //autoHeight : true,
+          transitionStyle : "fadeUp"
+        });
+
+        //Init progressBar where elem is $("#owl-demo")
+        function progressBar(elem){
+          $elem = elem;
+          //build progress bar elements
+          buildProgressBar();
+          //start counting
+          start();
+        }
+
+        //create div#progressBar and div#bar then append to $(".owl-carousel")
+        function buildProgressBar(){
+          $progressBar = $("<div>",{
+            id:"progressBar"
+          });
+          $bar = $("<div>",{
+            id:"bar"
+          });
+          $progressBar.append($bar).appendTo($elem);
+        }
+
+        function start() {
+          //reset timer
+          percentTime = 0;
+          isPause = false;
+          //run interval every 0.01 second
+          tick = setInterval(interval, 10);
+        };
+
+        function interval() {
+          if(isPause === false){
+            percentTime += 1 / time;
+            $bar.css({
+               width: percentTime+"%"
+             });
+            //if percentTime is equal or greater than 100
+            if(percentTime >= 100){
+              //slide to next item
+              $elem.trigger('owl.next')
+            }
+          }
+        }
+
+        //pause while dragging
+        function pauseOnDragging(){
+          isPause = true;
+        }
+
+        //moved callback
+        function moved(){
+          //clear interval
+          clearTimeout(tick);
+          //start again
+          start();
+        }
+    });
+
+    //Initiat WOW JS
+    new WOW().init();
 
 
-	var owl = $("#owl-ciekawostki");
+    $(document).ready(function() {
+        //Animated Progress
+        $('.progress-bar').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+            if (visible) {
+                $(this).css('width', $(this).data('width') + '%');
+                $(this).unbind('inview');
+            }
+        });
+
+        //Animated Number
+        $.fn.animateNumbers = function(stop, commas, duration, ease) {
+            return this.each(function() {
+                var $this = $(this);
+                var start = parseInt($this.text().replace(/,/g, ""));
+                commas = (commas === undefined) ? true : commas;
+                $({value: start}).animate({value: stop}, {
+                    duration: duration == undefined ? 1000 : duration,
+                    easing: ease == undefined ? "swing" : ease,
+                    step: function() {
+                        $this.text(Math.floor(this.value));
+                        if (commas) { $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")); }
+                    },
+                    complete: function() {
+                        if (parseInt($this.text()) !== stop) {
+                            $this.text(stop);
+                            if (commas) { $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")); }
+                        }
+                    }
+                });
+            });
+        };
+
+        $('.animated-number').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+            var $this = $(this);
+            if (visible) {
+                $this.animateNumbers($this.data('digit'), false, $this.data('duration'));
+                $this.unbind('inview');
+            }
+        });
+    });
+
+
+    var owl = $("#owl-ciekawostki");
 
     owl.owlCarousel({
         items : 4, //10 items above 1000px browser width
-  	  itemsDesktop : [1400,3], //5 items between 1000px and 901px
+        itemsDesktop : [1400,3], //5 items between 1000px and 901px
         itemsDesktopMedium : [1000,2], //5 items between 1000px and 901px
         itemsDesktopSmall : [900,1], // betweem 900px and 601px
         itemsTablet: [600,1], //2 items between 600 and 0
@@ -200,50 +200,50 @@ jQuery(function($) {'use strict';
 
 
 
-	$('.jumbotron h1')
-	  .textillate({ in: { effect: 'flipInY' } });
+    $('.jumbotron h1')
+      .textillate({ in: { effect: 'flipInY' } });
 
-	$('.jumbotron p')
-	  .textillate({ initialDelay: 1000, in: { delay: 1, shuffle: true } });
+    $('.jumbotron p')
+      .textillate({ initialDelay: 1000, in: { delay: 1, shuffle: true } });
 
-	setTimeout(function () {
-		$('.fade').addClass('in');
-	}, 250);
+    setTimeout(function () {
+        $('.fade').addClass('in');
+    }, 250);
 
-	setTimeout(function () {
-		$('h1.glow').removeClass('in');
-	}, 2000);
+    setTimeout(function () {
+        $('h1.glow').removeClass('in');
+    }, 2000);
 
 
-	$('.collapse').collapse({
-		 toggle: false
-	});
+    $('.collapse').collapse({
+         toggle: false
+    });
 
-	$('.collapse').on('shown.bs.collapse', function () {
-   		$('.collapse').not($(this)).collapse('hide');
-	});
+    $('.collapse').on('shown.bs.collapse', function () {
+           $('.collapse').not($(this)).collapse('hide');
+    });
 
-	//Google Map
-	var latitude = $('#google-map').data('latitude');
-	var longitude = $('#google-map').data('longitude');
-	function initialize_map() {
-		var myLatlng = new google.maps.LatLng(latitude,longitude);
-		var mapOptions = {
-			zoom: 14,
-			scrollwheel: false,
-			center: myLatlng
-		};
-		var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-		google.maps.event.addDomListener(window, 'resize', function() {
-		    var center = map.getCenter();
-		    google.maps.event.trigger(map, 'resize');
-		    map.setCenter(center); 
-		});
-		var marker = new google.maps.Marker({
-			position: myLatlng,
-			map: map
-		});
-	}
-	google.maps.event.addDomListener(window, 'load', initialize_map);
+    //Google Map
+    var latitude = $('#google-map').data('latitude');
+    var longitude = $('#google-map').data('longitude');
+    function initialize_map() {
+        var myLatlng = new google.maps.LatLng(latitude,longitude);
+        var mapOptions = {
+            zoom: 14,
+            scrollwheel: false,
+            center: myLatlng
+        };
+        var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+        google.maps.event.addDomListener(window, 'resize', function() {
+            var center = map.getCenter();
+            google.maps.event.trigger(map, 'resize');
+            map.setCenter(center); 
+        });
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map
+        });
+    }
+    google.maps.event.addDomListener(window, 'load', initialize_map);
 
 });
