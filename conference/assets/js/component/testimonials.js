0a0cf723c2d448540f04eb56bb4641fb3c0508ad
Stelmach, Krzysztof <Krzysztof.Stelmach@ihsmarkit.com>
2016-08-23 09:14:51 +0200

    Revert "Merge pull request #44 from IHS-Gdansk/master"
    This reverts commit ec7a689da44662591c323c7b93654636195dc2c9, reversing
    changes made to 1f1ba13c5c75bab5a6d18c12e52a1aeb0d9c1bed.

--- conference/assets/js/component/testimonials.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('#carousel-testimonials').owlCarousel({
-        autoPlay: true,
-        slideSpeed: 300,
-        paginationSpeed: 400,
-        singleItem: true
-    });
-});
