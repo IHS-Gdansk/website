11f26b3fb0875d819bd036ea0547e31e608bc698
Tomasz Heimowski <tomek.heimowski@gmail.com>
2017-10-13 16:57:30 +0200

    build from Razor template

--- build.fsx
+++ build.fsx
@@ -1,19 +1,32 @@
 #load "packages/FSharp.Formatting/FSharp.Formatting.fsx"
 #r @"packages/FAKE/tools/FakeLib.dll"
+#r @"packages/RazorEngine/lib/net45/RazorEngine.dll"
+
+open System
+open System.IO
 
 open Fake
+
+open RazorEngine
+open RazorEngine.Templating
 
 let content = __SOURCE_DIRECTORY__
 let output  = __SOURCE_DIRECTORY__ </> "output"
 
 let copyFiles () =
-  for dir in [ "css" ] do
+  for dir in [ "css"; "js"; "images"; "fonts" ] do
     ensureDirectory (output </> dir)
     CopyRecursive (content </> dir) (output </> dir) true
     |> Log "Copying: "
 
+type Website = 
+  { Title : string }
+
 let generate () =
-  ()
+  let template = File.ReadAllText (content </> "index.cshtml")
+  let website = { Title = "IHS Markit" }
+  let html = Engine.Razor.RunCompile(template, "key", null, website)
+  File.WriteAllText (output </> "index.html", html)
 
 Target "Generate" (fun _ ->
   copyFiles()
